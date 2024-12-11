73390edbb58f0616ed47f203572a8629ffd7cc43
Matt Harrison <hi@matt-harrison.com>
2024-12-11 08:50:50 +0000

    day11 part2

--- src/day11/mod.rs
+++ src/day11/mod.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 // 0 -> 1
 pub fn rule1(input: &i64) -> Option<i64> {
     if input == &0 {
@@ -24,27 +26,38 @@ pub fn rule3(input: &i64) -> Option<i64> {
     Some(*input * 2024)
 }
 
-pub fn apply_rules(input: &Vec<i64>) -> Vec<i64> {
-    let mut output = Vec::with_capacity(input.len() * 2);
-    for i in input {
-        if let Some(result) = rule1(i) {
-            output.push(result);
-        } else if let Some((lhs, rhs)) = rule2(i) {
-            output.push(lhs);
-            output.push(rhs);
-        } else if let Some(result) = rule3(i) {
-            output.push(result);
-        }
+pub fn apply_rules(input: &i64) -> Vec<i64> {
+    if let Some(result) = rule1(input) {
+        vec![result]
+    } else if let Some((lhs, rhs)) = rule2(input) {
+        vec![lhs, rhs]
+    } else if let Some(result) = rule3(input) {
+        vec![result]
+    } else {
+        unreachable!("No rule applied");
     }
-    output
 }
 
-pub fn blink(input: &Vec<i64>, n: usize) -> Vec<i64> {
-    let mut output = input.clone();
-    for i in 0..n {
-        output = apply_rules(&output);
+pub fn blink_recursive_count(
+    mut input: Vec<i64>,
+    n: usize,
+    hashmap: &mut HashMap<(i64, usize), usize>,
+) -> usize {
+    if n == 0 || input.len() == 0 {
+        return input.len();
     }
-    output
+
+    let rest = input.split_off(1);
+
+    let lhs = if hashmap.contains_key(&(input[0], n)) {
+        *hashmap.get(&(input[0], n)).unwrap()
+    } else {
+        let lhs = blink_recursive_count(apply_rules(&input[0]), n - 1, hashmap);
+        hashmap.insert((input[0], n), lhs);
+        lhs
+    };
+
+    lhs + blink_recursive_count(rest, n, hashmap)
 }
 
 pub fn parse_input(input: String) -> Vec<i64> {
@@ -56,47 +69,32 @@ pub fn parse_input(input: String) -> Vec<i64> {
 
 #[cfg(test)]
 mod tests {
+    use std::collections::HashMap;
+
     use super::*;
     use crate::util::parse as util_parse;
 
     #[test]
-    fn test_rule1() {
-        assert_eq!(rule1(&0), Some(1));
-        assert_eq!(rule1(&1), None);
-    }
-
-    #[test]
-    fn test_rule2() {
-        assert_eq!(rule2(&34), Some((3, 4)));
-        assert_eq!(rule2(&1234), Some((12, 34)));
-        assert_eq!(rule2(&678906), Some((678, 906)));
-        assert_eq!(rule2(&8704), Some((87, 4)));
-    }
-
-    #[test]
-    fn test_rule3() {
-        assert_eq!(rule3(&1), Some(2024));
-        assert_eq!(rule3(&2), Some(4048));
-    }
-
-    #[test]
     fn test_example_part1() {
         let input = util_parse::<Vec<i64>>("day11", "example.txt", parse_input);
-        let output = blink(&input, 25);
-        assert_eq!(output.len(), 55312);
+        let mut hashmap: HashMap<(i64, usize), usize> = HashMap::new();
+        let count = blink_recursive_count(input, 25, &mut hashmap);
+        assert_eq!(count, 55312);
     }
 
     #[test]
     fn test_part1() {
         let input = util_parse::<Vec<i64>>("day11", "puzzle.txt", parse_input);
-        let output = blink(&input, 25);
-        assert_eq!(output.len(), 233050);
+        let mut hashmap: HashMap<(i64, usize), usize> = HashMap::new();
+        let count = blink_recursive_count(input, 25, &mut hashmap);
+        assert_eq!(count, 233050);
     }
 
     #[test]
     fn test_part2() {
         let input = util_parse::<Vec<i64>>("day11", "puzzle.txt", parse_input);
-        let output = blink(&input, 75);
-        assert_eq!(output.len(), 233050);
+        let mut hashmap: HashMap<(i64, usize), usize> = HashMap::new();
+        let count = blink_recursive_count(input, 75, &mut hashmap);
+        assert_eq!(count, 276661131175807);
     }
 }
